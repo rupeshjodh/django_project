461ed8f9d565f40661d2c17407dc8f7498df8c28
satakshisaha <satakshisaha07@gmail.com>
2021-09-25 19:33:31 +0530

    faculty added

--- faculty/models.py
+++ faculty/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+# Create your models here.
+class Faculty(models.Model):
+    fno = models.IntegerField()
+    f_name = models.CharField(max_length=64)
+    f_sal = models.FloatField()
+    f_city = models.CharField(max_length=64)
+
+
+    class Meta:
+        db_table = "faculty"
+
+    def __str__(self):
+        return self.f_name
+
+
+from django.db import models
+
 # Create your models here.
--- faculty/views.py
+++ faculty/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.http import HttpResponse
+from faculty.models import Faculty
+# Create your views here.
+def faculty_info(request):
+    faculty={
+        'fno':100,
+        'f_name':"Khushboo Trivedi",
+        'f_sal':100000,
+        'f_city':"Pune"
+    }
+    resp='<h1>Faulty Number:{}<br>Faculty Name:{}<br>Faculty Salary:{}<br>Faculty City:{}</' \
+         'h1>'.format(faculty['fno'],faculty['f_name'],faculty['f_sal'],faculty['f_city'])
+    return HttpResponse(resp)
+
+def faculty_info1(request):
+    faculty={
+        'fno':100,
+        'f_name':"Khushboo Trivedi",
+        'f_sal':100000,
+        'f_city':"Pune"
+    }
+    json_data=json.dumps(faculty)
+    return HttpResponse(json_data,content_type='application/json')
+
+def faculty_info2(request):
+    faculty={
+        'fno':100,
+        'f_name':"Khushboo Trivedi",
+        'f_sal':100000,
+        'f_city':"Pune"
+    }
+    return JsonResponse(faculty)
+
 
 # Create your views here.
+def faculty_info_view(request):
+    faculty=Faculty.objects.all()
+    print(faculty)
+    return render(request,'faculty/results.html',{'faculty':faculty})
+
